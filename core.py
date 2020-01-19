59894ddf0852101ba21beafe96596f7654a16adf
sunshineplan <sunshineplan@users.noreply.github.com>
2020-01-19 14:43:13 +0800

    If key is empty, then use base64 encode directly

--- core.py
+++ core.py
@@ -5,21 +5,27 @@ from base64 import b64decode, b64encode
 
 
 def decrypt(key, content):
-    dc = ''
     try:
         c = b64decode(content).decode()
     except:
         return None
-    for i in range(len(c)):
-        k = key[i % len(key)]
-        dc += chr((1114111*2 + ord(c[i]) -
-                   ord(k) - ord(str(i)[-1])) % 1114111)
-    return dc
+    if key == '':
+        return c
+    else:
+        dc = ''
+        for i in range(len(c)):
+            k = key[i % len(key)]
+            dc += chr((1114111*2 + ord(c[i]) -
+                    ord(k) - ord(str(i)[-1])) % 1114111)
+        return dc
 
 
 def encrypt(key, content):
-    ec = ''
-    for i in range(len(content)):
-        k = key[i % len(key)]
-        ec += chr((ord(content[i]) + ord(k) + ord(str(i)[-1])) % 1114111)
-    return b64encode(ec.encode()).decode()
+    if key == '':
+        return b64encode(content.encode()).decode()
+    else:
+        ec = ''
+        for i in range(len(content)):
+            k = key[i % len(key)]
+            ec += chr((ord(content[i]) + ord(k) + ord(str(i)[-1])) % 1114111)
+        return b64encode(ec.encode()).decode()
