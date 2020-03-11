d9dc5bfe14b8c4e33f53105a86b497c8aa813895
sunshineplan <sunshineplan@users.noreply.github.com>
2020-03-11 20:47:20 +0800

    Use byte array instead of charset when encrypting

--- core.py
+++ core.py
@@ -4,7 +4,9 @@
 from base64 import b64decode, b64encode
 
 
-def get_loop(key, content, loop):
+def preprocess(key, content, loop):
+    if isinstance(content, str):
+        content = content.encode()
     try:
         loop = int(loop)
         if loop < 0 or loop > 99999:
@@ -13,12 +15,12 @@ def get_loop(key, content, loop):
         loop = int(str(len(content)/len(key))[-1])
         if not loop:
             loop = int(str(len(content))[0])
-    return loop
+    return key.encode(), content, loop
 
 
 def decrypt(key, content, loop='auto'):
     try:
-        content = b64decode(content).decode()
+        content = b64decode(content)
     except:
         return None
     if content == '':
@@ -27,15 +29,14 @@ def decrypt(key, content, loop='auto'):
         return content
     else:
         key = key[:1000]
-        loop = get_loop(key, content, loop)
+        key, content, loop = preprocess(key, content, loop)
         for _ in range(loop):
-            dc = ''
+            dc = b''
             for i in range(len(content)):
                 k = key[i % len(key)]
-                dc += chr((ord(content[i]) - ord(k) -
-                           ord(str(i)[-1]) + 1114111*2) % 1114111)
+                dc += bytes([(content[i] - k - int(str(i)[-1]) + 512) % 256])
             content = dc
-        return content
+        return content.decode()
 
 
 def encrypt(key, content, loop='auto'):
@@ -45,12 +46,11 @@ def encrypt(key, content, loop='auto'):
         return b64encode(content.encode()).decode()
     else:
         key = key[:1000]
-        loop = get_loop(key, content, loop)
+        key, content, loop = preprocess(key, content, loop)
         for _ in range(loop):
-            ec = ''
+            ec = b''
             for i in range(len(content)):
                 k = key[i % len(key)]
-                ec += chr((ord(content[i]) + ord(k) +
-                           ord(str(i)[-1])) % 1114111)
+                ec += bytes([(content[i] + k + int(str(i)[-1])) % 256])
             content = ec
-        return b64encode(content.encode()).decode()
+        return b64encode(content).decode()
