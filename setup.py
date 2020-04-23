b654df9d65866ca0ed70c6eea005bf0e7657aa4e
sunshineplan <sunshineplan@users.noreply.github.com>
2020-04-23 11:08:34 +0800

    Update install requires

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ste',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=['flask', 'click'],
+    install_requires=['flask', 'click', 'cryptodomex'],
     entry_points={
         'console_scripts': [
             'ste=ste.cli:main',
